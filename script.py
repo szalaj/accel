6895899f284fe524be01274a21eaec6a88f7ebe3
szalaj <mszalajski@gmail.com>
2022-10-16 21:55:49 +0200

    read file

--- script.py
+++ script.py
@@ -1,7 +1,16 @@
+import os
+import bpy
+import sys
+
+
+
+os.chdir(os.path.dirname(bpy.path.abspath("//")))
+
+
 
 ans = []
  
-# open .tsv file
+
 with open("data/noname-1665346695947.tsv") as f:
    
   # Read data line by line
@@ -12,6 +21,10 @@ with open("data/noname-1665346695947.tsv") as f:
     l=line.split('\t')
      
     ans.append((float(l[2].replace(',','.')), float(l[3].replace(',','.')), float(l[4].replace(',','.'))))
- 
-
 print(ans)
+
+obj = bpy.context.object
+
+for i,a in enumerate(ans):
+    obj.location[2] = a[2]
+    obj.keyframe_insert(data_path="location", frame=i*10, index=2)

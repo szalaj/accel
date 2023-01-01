0c554aeac22077334acf6c0c4208313020811b4e
szalaj <mszalajski@gmail.com>
2023-01-01 22:27:36 +0100

    baza danych

--- read4.py
+++ read4.py
@@ -3,13 +3,17 @@ import numpy as np
 import socket, errno, time
 import matplotlib.pyplot as plt
 import datetime
+import psycopg2
 
 
 UDP_IP = "192.168.0.3"
 UDP_PORT = 5005
 
 print('start')
-   
+conn = psycopg2.connect(dbname="postgres", user="postgres", password="Robak123", host="localhost", port=5432)
+
+cur = conn.cursor()
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 #sock.setblocking(False)
@@ -22,9 +26,12 @@ for i in range(200):
 
         dataraw = data.decode('utf-8').split(',')
 
+        ts = datetime.datetime.now()
         ax = dataraw[2].strip()
         ay = dataraw[3].strip()
         az = dataraw[4].strip()
+
+        cur.execute("INSERT INTO accel (ts, ax, ay, az) VALUES (%s, %s, %s, %s)", (ts, ax, ay, az))
         
         log = "ax: {} , ay: {} , az: {}".format(ax,ay,az)
         print(log)
@@ -38,3 +45,7 @@ for i in range(200):
             print(e)
             break
     
+conn.commit()
+
+cur.close()
+conn.close()
